6e42166ed99f7a5d0f195fd270282a604a2d3abb
Moisey <Mosha.site>
2020-11-26 19:00:25 +0300

    add yandex static

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,8 @@ export default {
 
     // Global CSS (https://go.nuxtjs.dev/config-css)
     css: [
-        'vuesax/dist/vuesax.css'
+        'vuesax/dist/vuesax.css',
+        'boxicons/css/boxicons.min.css'
     ],
 
     // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
